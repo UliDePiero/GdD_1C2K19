8ca1aef9aa0f9ab47cbab22bda602c448e928f40
Tomas83 <37963301+Tomas83@users.noreply.github.com>
2019-06-04 13:53:12 -0300

    Aca esta echo el login

--- Inicio y Log/Login.cs
+++ Inicio y Log/Login.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace FrbaCrucero
 {
@@ -19,11 +21,61 @@ namespace FrbaCrucero
 
         private void iniciar_Click(object sender, EventArgs e)
         {
+            logins(textBoxUsuario.Text, textBoxContraseña.Text);
+            /*
             this.Close();
             Menu form = new Menu();
-            form.Show();
+            form.Show();*/
         }
 
-        
+
+        private static string server = ConfigurationManager.AppSettings["server"].ToString();
+        private static string user = ConfigurationManager.AppSettings["user"].ToString();
+        private static string password = ConfigurationManager.AppSettings["password"].ToString();
+
+        public static SqlConnection getConnection()
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "SERVER=" + server + "\\SQLSERVER2012; DATABASE = GD1C2019;UID=" + user + ";PASSWORD=" + password + ";";
+            conn.Open();
+            return conn;
+        }
+
+
+        void logins(string usua, string contra)
+        {
+            try
+            {
+                using (SqlConnection conexion = getConnection())
+                {
+                    //string comando = "SELECT usua_nombre, usua_password FROM PENSAMIENTO_LINEAL.Usuario WHERE usua_nombre = '" + usua + "' AND  usua_password = HASHBYTES('SHA2_256','" + contra + "')";
+
+                    string comando = "SELECT usua_nombre, usua_password FROM PENSAMIENTO_LINEAL.Usuario WHERE usua_nombre = '" + usua + "' AND  usua_password = HASHBYTES('SHA2_256','" + contra + "')";
+                    using (SqlCommand cmd = new SqlCommand(comando, conexion))
+                    {
+                        SqlDataReader dr = cmd.ExecuteReader();
+                        if (dr.Read())
+                        {
+                            MessageBox.Show("Login exitoso.");
+                            conexion.Close();
+                            this.Close();
+                            Menu form = new Menu();
+                            form.Show();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Datos incorrectos.");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("no funca");
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+
     }
 }
--- TriggerCodificarContra.sql
+++ TriggerCodificarContra.sql
@@ -5,7 +5,7 @@ on PENSAMIENTO_LINEAL.Usuario
 instead of insert
 as
 begin
-	insert into PENSAMIENTO_LINEAL.Usuario
+	insert into PENSAMIENTO_LINEAL.Usuario ()
 	select usua_nombre, usua_apellido,usua_direccion,usua_mail, HASHBYTES('SHA2_256',usua_password), usua_telefono, usua_documento, usua_fechanac
 	from inserted
 end

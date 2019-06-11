7fcf345a5ade0737f4e22ba88ed6f81363313fd7
Uli <udepiero@alumno.huergo.edu.ar>
2019-06-11 17:29:11 -0300

    asignar roles sql

---
+++ AsignarRolesAdministradores.sql
@@ -0,0 +1,6 @@
+SELECT usua_username, usua_id FROM PENSAMIENTO_LINEAL.Usuario WHERE usua_username IS NOT NULL
+INSERT INTO PENSAMIENTO_LINEAL.Rol_Usuario(rol_usuario_usuarioid, rol_usuario_rolid) VALUES (245710, 1)
+INSERT INTO PENSAMIENTO_LINEAL.Rol_Usuario(rol_usuario_usuarioid, rol_usuario_rolid) VALUES (245711, 1)
+INSERT INTO PENSAMIENTO_LINEAL.Rol_Usuario(rol_usuario_usuarioid, rol_usuario_rolid) VALUES (245712, 1)
+INSERT INTO PENSAMIENTO_LINEAL.Rol_Usuario(rol_usuario_usuarioid, rol_usuario_rolid) VALUES (245713, 1)
+INSERT INTO PENSAMIENTO_LINEAL.Rol_Usuario(rol_usuario_usuarioid, rol_usuario_rolid) VALUES (245714, 1)

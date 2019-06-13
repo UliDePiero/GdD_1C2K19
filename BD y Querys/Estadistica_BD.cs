6f15ec37b30a5edc85245caea8e36963028e2301
cimimuxmaio <cimimuxmaio2@gmail.com>
2019-06-13 10:27:50 -0300

    Correcciones - Estadistica

--- BD y Querys/Estadistica_BD.cs
+++ BD y Querys/Estadistica_BD.cs
@@ -17,10 +17,10 @@ namespace FrbaCrucero.BD_y_Querys
             switch (estadistica)
             {
                 case Estadistica.CRUCEROS_CON_MAS_DIAS_INHABILES:
-                    query = string.Format(@"select TOP 5 cruc_id as ID, cruc_identificador as IDENTIFICADOR, sum(DATEDIFF(day, esta_fechabaja, esta_fechaalta)) as DIAS_FUERA_DE_SERVICIO
+                    query = string.Format(@"select TOP 5 cruc_id as ID, cruc_identificador as IDENTIFICADOR, sum(dbo.diasFuera(" + anio + @", '"+ semestre +@"', esta_fechabaja, esta_fechaalta)) as DIAS_FUERA_DE_SERVICIO
                                             from PENSAMIENTO_LINEAL.Crucero
                                             	join PENSAMIENTO_LINEAL.Estado_crucero on (cruc_id = esta_crucero)
-                                            where month(esta_fechaalta)" + semestre + @" AND year(esta_fechaalta) = " + anio + @"
+                                            where month(esta_fechaalta) " + semestre + @" AND year(esta_fechaalta) = " + anio + @"
                                             group by cruc_id, cruc_identificador
                                             order by 3 desc");
                     break;
@@ -30,7 +30,7 @@ namespace FrbaCrucero.BD_y_Querys
                     query = string.Format(@"select TOP 5 reco_cruc_recoid as ID, count(pasa_id) as CANTIDAD_VENDIDOS, sum(pasa_precio) as GANANCIA
                                             from PENSAMIENTO_LINEAL.Recorrido_crucero
                                             	join PENSAMIENTO_LINEAL.Pasaje on (reco_cruc_id = pasa_viaje)
-                                            where month(pasa_fecha) " + semestre + @" AND year(pasa_fecha) = "+ anio + @"
+                                            where month(pasa_fecha) " + semestre + " AND year(pasa_fecha) = " + anio + @"
                                             group by reco_cruc_recoid
                                             order by 2 desc");
                     break;

c1d27a990fa77f2ac150d11a08ef0d85ad0b1cc0
Uli <udepiero@alumno.huergo.edu.ar>
2019-06-14 18:54:23 -0300

    ABM Puerto

--- BD y Querys/Puerto_BD.cs
+++ BD y Querys/Puerto_BD.cs
@@ -15,9 +15,9 @@ namespace FrbaCrucero.BD_y_Querys
     {
         public static void cargar_grilla_puertos(DataGridView grillaPuertos)
         {
-            string query = string.Format(@"SELECT puer_nombre as Puerto
+            string query = string.Format(@"SELECT puer_id as ID, puer_nombre as Puerto
                                            FROM PENSAMIENTO_LINEAL.Puerto
-                                           ORDER BY 1 ASC");
+                                           ORDER BY 2 ASC");
             DBConnection.llenar_grilla(grillaPuertos, query);
         }
 
@@ -81,5 +81,154 @@ namespace FrbaCrucero.BD_y_Querys
             conn.Dispose();
             return puerto;
         }
+
+        public static void cargar_grilla_puertos_recorridos(DataGridView grillaPuertos)
+        {
+            string query = string.Format(@"SELECT DISTINCT(puer_id) as ID, puer_nombre as Puerto
+                                           FROM PENSAMIENTO_LINEAL.Puerto 
+										   JOIN PENSAMIENTO_LINEAL.Tramo t1 ON(puer_id=t1.tram_origen)
+										   JOIN PENSAMIENTO_LINEAL.Tramo t2 ON(puer_id=t2.tram_destino)
+										   JOIN PENSAMIENTO_LINEAL.Recorrido_tramo ON(t1.tram_id=reco_tram_tramid OR t2.tram_id=reco_tram_tramid)
+                                           ORDER BY 2 ASC");
+            DBConnection.llenar_grilla(grillaPuertos, query);
+        }
+
+        public static void cargar_grilla_puertos_viajes(DataGridView grillaPuertos)
+        {
+            string query = string.Format(@"SELECT DISTINCT(puer_id) as ID, puer_nombre as Puerto
+                                           FROM PENSAMIENTO_LINEAL.Puerto 
+										   JOIN PENSAMIENTO_LINEAL.Tramo t1 ON(puer_id=t1.tram_origen)
+										   JOIN PENSAMIENTO_LINEAL.Tramo t2 ON(puer_id=t2.tram_destino)
+										   JOIN PENSAMIENTO_LINEAL.Recorrido_tramo ON(t1.tram_id=reco_tram_tramid OR t2.tram_id=reco_tram_tramid)
+										   JOIN PENSAMIENTO_LINEAL.Recorrido_crucero ON(reco_cruc_recoid=reco_tram_recoid)
+                                           ORDER BY 2 ASC");
+            DBConnection.llenar_grilla(grillaPuertos, query);
+        }
+
+        public static void cargar_grilla_puertos_pasajes(DataGridView grillaPuertos) //TARDA MUCHO //TARDA MUCHO //TARDA MUCHO 
+        {
+            string query = string.Format(@"SELECT DISTINCT(puer_id) as ID, puer_nombre as Puerto
+                                           FROM PENSAMIENTO_LINEAL.Puerto 
+										   JOIN PENSAMIENTO_LINEAL.Tramo t1 ON(puer_id=t1.tram_origen)
+										   JOIN PENSAMIENTO_LINEAL.Tramo t2 ON(puer_id=t2.tram_destino)
+										   JOIN PENSAMIENTO_LINEAL.Recorrido_tramo ON(t1.tram_id=reco_tram_tramid OR t2.tram_id=reco_tram_tramid)
+										   JOIN PENSAMIENTO_LINEAL.Recorrido_crucero ON(reco_cruc_recoid=reco_tram_recoid)
+										   JOIN PENSAMIENTO_LINEAL.Pasaje ON(pasa_viaje=reco_cruc_id)
+                                           ORDER BY 2 ASC");
+            DBConnection.llenar_grilla(grillaPuertos, query);
+        }
+
+        public static void cargar_grilla_puertos_no_recorridos(DataGridView grillaPuertos)
+        {
+            string query = string.Format(@"SELECT DISTINCT(puer_id) as ID, puer_nombre as Puerto
+                                           FROM PENSAMIENTO_LINEAL.Puerto
+										   WHERE puer_id NOT IN (SELECT puer_id
+																FROM PENSAMIENTO_LINEAL.Puerto 
+																JOIN PENSAMIENTO_LINEAL.Tramo t1 ON(puer_id=t1.tram_origen)
+																JOIN PENSAMIENTO_LINEAL.Tramo t2 ON(puer_id=t2.tram_destino)
+																JOIN PENSAMIENTO_LINEAL.Recorrido_tramo ON(t1.tram_id=reco_tram_tramid OR t2.tram_id=reco_tram_tramid))
+                                           ORDER BY 2 ASC");
+            DBConnection.llenar_grilla(grillaPuertos, query);
+        }
+
+        public static void cargar_grilla_puertos_no_viajes(DataGridView grillaPuertos)
+        {
+            string query = string.Format(@"SELECT DISTINCT(puer_id) as ID, puer_nombre as Puerto
+                                           FROM PENSAMIENTO_LINEAL.Puerto
+										   WHERE puer_id NOT IN (SELECT puer_id
+                                                                FROM PENSAMIENTO_LINEAL.Puerto 
+										                        JOIN PENSAMIENTO_LINEAL.Tramo t1 ON(puer_id=t1.tram_origen)
+										                        JOIN PENSAMIENTO_LINEAL.Tramo t2 ON(puer_id=t2.tram_destino)
+										                        JOIN PENSAMIENTO_LINEAL.Recorrido_tramo ON(t1.tram_id=reco_tram_tramid OR t2.tram_id=reco_tram_tramid)
+										                        JOIN PENSAMIENTO_LINEAL.Recorrido_crucero ON(reco_cruc_recoid=reco_tram_recoid))
+                                           ORDER BY 2 ASC");
+            DBConnection.llenar_grilla(grillaPuertos, query);
+        }
+
+        public static void cargar_grilla_puertos_no_pasajes(DataGridView grillaPuertos) //TARDA MUCHO //TARDA MUCHO //TARDA MUCHO 
+        {
+            string query = string.Format(@"SELECT DISTINCT(puer_id) as ID, puer_nombre as Puerto
+                                           FROM PENSAMIENTO_LINEAL.Puerto
+										   WHERE puer_id NOT IN (SELECT puer_id
+                                                                FROM PENSAMIENTO_LINEAL.Puerto 
+										                        JOIN PENSAMIENTO_LINEAL.Tramo t1 ON(puer_id=t1.tram_origen)
+										                        JOIN PENSAMIENTO_LINEAL.Tramo t2 ON(puer_id=t2.tram_destino)
+										                        JOIN PENSAMIENTO_LINEAL.Recorrido_tramo ON(t1.tram_id=reco_tram_tramid OR t2.tram_id=reco_tram_tramid)
+										                        JOIN PENSAMIENTO_LINEAL.Recorrido_crucero ON(reco_cruc_recoid=reco_tram_recoid)
+										                        JOIN PENSAMIENTO_LINEAL.Pasaje ON(pasa_viaje=reco_cruc_id))
+                                           ORDER BY 2 ASC");
+            DBConnection.llenar_grilla(grillaPuertos, query);
+        }
+
+        public static bool nuevo_puerto(string puerto)
+        {
+            try
+            {
+                string query = string.Format(@"INSERT INTO PENSAMIENTO_LINEAL.Puerto(puer_nombre) VALUES (@puer_nombre)");
+                SqlConnection conn = DBConnection.getConnection();
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@puer_nombre", puerto);
+
+                cmd.ExecuteNonQuery();
+                
+                cmd.Dispose();
+                conn.Close();
+                conn.Dispose();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error al agregar puerto", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return false;
+        }
+
+        public static bool modificar_puerto(string nombre, Puerto puerto)
+        {
+            try
+            {
+                string query = string.Format(@"UPDATE INTO PENSAMIENTO_LINEAL.Puerto SET puer_nombre=@puer_nombre WHERE puer_id=@puer_id");
+                SqlConnection conn = DBConnection.getConnection();
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@puer_id", puerto.id);
+                cmd.Parameters.AddWithValue("@puer_nombre", nombre);
+
+                cmd.ExecuteNonQuery();
+
+                cmd.Dispose();
+                conn.Close();
+                conn.Dispose();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error al modificar puerto", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return false;
+        }
+
+        public static bool eliminar_puerto(Puerto puerto)
+        {
+            try
+            {
+                string query = string.Format(@"chasquearDedos (@puer_id)");
+                SqlConnection conn = DBConnection.getConnection();
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@puer_id", puerto.id);                
+
+                cmd.ExecuteNonQuery();
+
+                cmd.Dispose();
+                conn.Close();
+                conn.Dispose();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error al eliminar puerto", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return false;
+        }
+
     }
 }

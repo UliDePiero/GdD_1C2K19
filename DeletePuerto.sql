c297fd603023ab26fcbe2e252d881a9860cb49a2
Tomas83 <37963301+Tomas83@users.noreply.github.com>
2019-06-24 16:17:40 -0300

    Delete Puero

--- DeletePuerto.sql
+++ DeletePuerto.sql
@@ -1,28 +1,44 @@
 Use [GD1C2019]
 go
-create Procedure chasquearDedos (@puerto int)
+CREATE Procedure chasquearDedos (@puerto int)
 as
 begin
 Declare @Todo table (tramo int,reco int,recoCruc int) 
 Insert into @Todo
 select tram_id, reco_id, reco_cruc_id
-from PENSAMIENTO_LINEAL.Tramo join 
-	 PENSAMIENTO_LINEAL.Recorrido_tramo on (reco_tram_tramid = tram_id) join
-	 PENSAMIENTO_LINEAL.Recorrido on (reco_tram_recoid= reco_id) join
-	 PENSAMIENTO_LINEAL.Recorrido_crucero on (reco_cruc_id= reco_id) join
-	 PENSAMIENTO_LINEAL.Pasaje on (reco_cruc_id =pasa_viaje)
+from PENSAMIENTO_LINEAL.Tramo LEFT join 
+	 PENSAMIENTO_LINEAL.Recorrido_tramo on (reco_tram_tramid = tram_id) LEFT join
+	 PENSAMIENTO_LINEAL.Recorrido on (reco_tram_recoid= reco_id or reco_primertramo = tram_id) LEFT join
+	 PENSAMIENTO_LINEAL.Recorrido_crucero on (reco_cruc_recoid = reco_id)
 	 where @puerto = tram_destino OR @puerto = tram_origen
+	 
+Declare @tramos table (tramo int)
+insert @tramos select distinct tramo from @Todo
+Declare @recorridos table (reco int)
+insert @recorridos select distinct reco from @Todo
+Declare @viaje table (recoCruc int)
+insert @viaje select distinct recoCruc from @Todo
+
+
+
+delete PENSAMIENTO_LINEAL.Reserva
+from @viaje
+where rese_viaje = recoCruc
 delete PENSAMIENTO_LINEAL.Pasaje
-From PENSAMIENTO_LINEAL.Pasaje join @Todo on (pasa_viaje = recoCruc)
+from @viaje
+where pasa_viaje = recoCruc
 delete PENSAMIENTO_LINEAL.Recorrido_crucero
-From PENSAMIENTO_LINEAL.Recorrido_crucero join @Todo on (reco_cruc_id = recoCruc)
+from @viaje
+where reco_cruc_id = recoCruc
 delete PENSAMIENTO_LINEAL.Recorrido
-from PENSAMIENTO_LINEAL.Recorrido join @Todo on (reco_id = reco)
+from @recorridos
+where reco_id = reco
 delete PENSAMIENTO_LINEAL.Recorrido_tramo
-from PENSAMIENTO_LINEAL.Recorrido_tramo join @Todo on (tramo = reco_tram_tramid)
+from @tramos
+where reco_tram_tramid = tramo
 delete PENSAMIENTO_LINEAL.Tramo
-from PENSAMIENTO_LINEAL.Tramo join @Todo on (tramo = tram_id)
+from @tramos
+where tram_id = tramo
 delete PENSAMIENTO_LINEAL.Puerto
 where @puerto = puer_id
 end
-

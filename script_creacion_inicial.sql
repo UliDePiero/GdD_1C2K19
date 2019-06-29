396cc1b4127bfd89c5339b2403062865456fc060
Paniaton <lpanizza@est.frba.utn.edu.ar>
2019-06-29 11:32:23 -0300

    script con servicios

--- script_creacion_inicial.sql
+++ script_creacion_inicial.sql
@@ -738,7 +738,83 @@ delete PENSAMIENTO_LINEAL.Puerto
 where @puerto = puer_id
 end
 
-
 GO
 
+insert into PENSAMIENTO_LINEAL.Rol(rol_nombre,rol_habilitado)
+values ('Cliente',1)
+
+insert into PENSAMIENTO_LINEAL.Funcionalidad(func_nombre)
+values ('Reserva de pasaje')
+
+insert into PENSAMIENTO_LINEAL.Funcionalidad(func_nombre)
+values ('Pago de pasajes')
+
+insert into PENSAMIENTO_LINEAL.Rol(rol_nombre,rol_habilitado)
+values ('Administrador General',1)
+
+insert into PENSAMIENTO_LINEAL.Funcionalidad(func_nombre)
+values ('ABM Crucero')
+
+insert into PENSAMIENTO_LINEAL.Funcionalidad(func_nombre)
+values ('ABM Puerto')
+
+insert into PENSAMIENTO_LINEAL.Funcionalidad(func_nombre)
+values ('ABM Recorrido')
+
+insert into PENSAMIENTO_LINEAL.Funcionalidad(func_nombre)
+values ('ABM Rol')
+
+insert into PENSAMIENTO_LINEAL.Funcionalidad(func_nombre)
+values ('Listado Estadístico')
+
+insert into PENSAMIENTO_LINEAL.Funcionalidad(func_nombre)
+values ('Generar Viaje')
+
+insert into PENSAMIENTO_LINEAL.Rol_Funcion(rol_func_rolid,rol_func_funcid)
+values(1,1)
+
+insert into PENSAMIENTO_LINEAL.Rol_Funcion(rol_func_rolid,rol_func_funcid)
+values(1,2)
+
+insert into PENSAMIENTO_LINEAL.Rol_Funcion(rol_func_rolid,rol_func_funcid)
+values(2,1)
+
+insert into PENSAMIENTO_LINEAL.Rol_Funcion(rol_func_rolid,rol_func_funcid)
+values(2,2)
+
+insert into PENSAMIENTO_LINEAL.Rol_Funcion(rol_func_rolid,rol_func_funcid)
+values(2,3)
+
+insert into PENSAMIENTO_LINEAL.Rol_Funcion(rol_func_rolid,rol_func_funcid)
+values(2,4)
+
+insert into PENSAMIENTO_LINEAL.Rol_Funcion(rol_func_rolid,rol_func_funcid)
+values(2,5)
+
+insert into PENSAMIENTO_LINEAL.Rol_Funcion(rol_func_rolid,rol_func_funcid)
+values(2,6)
+
+insert into PENSAMIENTO_LINEAL.Rol_Funcion(rol_func_rolid,rol_func_funcid)
+values(2,7)
+
+insert into PENSAMIENTO_LINEAL.Rol_Funcion(rol_func_rolid,rol_func_funcid)
+values(2,8)
+
+insert into PENSAMIENTO_LINEAL.Servicio(serv_desc)
+values('Pileta')
+
+insert into PENSAMIENTO_LINEAL.Servicio(serv_desc)
+values('Teatro')
+
+insert into PENSAMIENTO_LINEAL.Servicio(serv_desc)
+values('Cine')
+
+insert into PENSAMIENTO_LINEAL.Servicio(serv_desc)
+values('Restaurant tematico')
+
+insert into PENSAMIENTO_LINEAL.Servicio(serv_desc)
+values('Tobogan de agua')
+
+insert into PENSAMIENTO_LINEAL.Servicio(serv_desc)
+values('Spa')
 
